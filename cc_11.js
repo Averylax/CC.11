89f149d35076a7b073d100de132e1c7bb3f20380
avery.white <averydouglascwhite@usf.edu>
2025-02-26 15:05:43 -0500

    Task 4 - Implemented Book Borrowing System

--- cc_11.js
+++ cc_11.js
@@ -18,7 +18,7 @@ class Book {
 }
 
 // Test Case
-const book1 = new Book('The Alchemist', 'Paulo Coelho', '1337', 5); // Task 1
+const book1 = new Book('The Alchemist', 'Paulo Coelho', 1337, 5); // Task 1
 console.log(book1.getDetails()); // Task 1
 book1.updateCopies(-1); // Task 1
 console.log(book1.getDetails()); // Task 1
@@ -43,7 +43,7 @@ class Borrower {
 }
 
 // Test Case
-const borrower1 = new Borrower('John Doe', '1234'); // Task 2
+const borrower1 = new Borrower('John Doe', 1234); // Task 2
 borrower1.borrowBook(`The Alchemist`); // Task 2
 console.log(borrower1.borrowedBooks); // Task 2
 borrower1.returnBook(`The Alchemist`); // Task 2
@@ -65,11 +65,30 @@ class Library {
     listBooks() {
         this.books.forEach(book => console.log(book.getDetails()));
     }
+    
+    // Task 4 - Implementing Book Borrowing System  
+    lendBook(borrowerId, isbn) {
+        const book = this.books.find(b => b.isbn === isbn && b.copies > 0);
+        const borrower = this.borrowers.find(b => b.borrowId === borrowerId);
+
+        if (book && borrower) {
+            book.updateCopies(-1);
+            borrower.borrowBook(book.title);
+        } else {
+            console.log(`Book not found or borrower not found`);}
+    }
 }
 
 // Test Case
 const library = new Library(); // Task 3
 library.addBook(book1); // Task 3
+library.borrowers.push(borrower1); // Task 3
 library.listBooks(); // Task 3
 
 console.log(`---------- Task 3 - End ----------`)
+
+// Task 4 - Implementing Book Borrowing System
+// Test Case
+library.lendBook(1234, 1337); // Task 4
+console.log(book1.getDetails()); // Task 4
+console.log(borrower1.borrowedBooks); // Task 4
